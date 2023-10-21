797c8ee0c719f4b56819ae222cdab7d7eebcc0a4
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-10-21 17:28:43 -0400

    NavBar component added using Bootstrap components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <></>;
+  const [contentType, setContentType] = useState("XML");
+
+  const handleContentTypeChange = (newContentType: string) => {
+    setContentType(newContentType);
+  };
+
+  return (
+    <div className="container-fluid">
+      <NavBar onContentTypeChange={handleContentTypeChange} />
+      <div className="row">
+        <div className="col-md-6">{/* Left panel content */}</div>
+        <div className="col-md-6">{/* Right panel content */}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,49 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Navbar, Dropdown, DropdownButton } from "react-bootstrap";
+import { GitHub } from "react-feather"; // Importing GitHub icon from react-feather
+import logo from "../assets/query.png";
+
+interface NavBarProps {
+  onContentTypeChange: (contentType: string) => void;
+}
+
+const NavBar = ({ onContentTypeChange }: NavBarProps) => {
+  const [selectedContentType, setSelectedContentType] = useState("XML");
+
+  const handleContentTypeChange = (contentType: string | null) => {
+    if (contentType) {
+      setSelectedContentType(contentType);
+      onContentTypeChange(contentType); // Propagate the change to parent component
+    }
+  };
+
+  return (
+    <Navbar bg="dark" variant="dark" fixed="top">
+      <Navbar.Brand href="#home">
+        <img src={logo} alt="QueryFusion Logo" height="30" />
+        QueryFusion
+      </Navbar.Brand>
+      <DropdownButton
+        id="dropdown-basic-button"
+        title={selectedContentType}
+        variant="secondary"
+        onSelect={handleContentTypeChange}>
+        <Dropdown.Item eventKey="XML">XML</Dropdown.Item>
+        <Dropdown.Item eventKey="JSON">JSON</Dropdown.Item>
+      </DropdownButton>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          <a
+            href="https://github.com/your-username/your-repo"
+            target="_blank"
+            rel="noopener noreferrer">
+            <GitHub size={24} />
+          </a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
