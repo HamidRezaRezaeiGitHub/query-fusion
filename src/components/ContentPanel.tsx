5a5e95fb067ace3c918f356efff2b07ed2c74d7f
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-11-05 19:47:23 -0500

    react-ace and ace-builds installed + editor added to the ContentPanel

--- src/components/ContentPanel.tsx
+++ src/components/ContentPanel.tsx
@@ -1,4 +1,10 @@
 import { ContentType } from "../types/ContentType";
+import "../styles/ContentPanel.css";
+import "../styles/debug.css";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/mode-xml";
+import "ace-builds/src-noconflict/theme-monokai";
 
 interface ContentPanelProps {
   contentType: ContentType;
@@ -6,10 +12,46 @@ interface ContentPanelProps {
 }
 
 const ContentPanel = ({ contentType, isDarkMode }: ContentPanelProps) => {
+  const editorMode = contentType === ContentType.JSON ? "json" : "xml";
+
+  const onLoad = () => {
+    console.log("Editor loaded");
+  };
+
+  const onChange = (newValue: string) => {
+    console.log("Change", newValue);
+  };
+
   return (
-    <div>
-      contentType: {contentType} and isDarkMode: {String(isDarkMode)}
-    </div>
+    <>
+      <div className="content-container debug-border-green-lime">
+        <AceEditor
+          placeholder={`Copy your ${editorMode} content here...`}
+          mode={editorMode}
+          theme="monokai"
+          name="editor"
+          onLoad={onLoad}
+          onChange={onChange}
+          fontSize={14}
+          showPrintMargin={false}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={``}
+          height="100%"
+          width="100%"
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 4,
+          }}
+        />
+      </div>
+      <div className="buttons-container debug-border-black-gray">
+        Buttons Container
+      </div>
+    </>
   );
 };
 
