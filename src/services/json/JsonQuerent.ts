c9cfee584eec24cdd55ac9875c77840d1f090004
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-12-26 19:12:16 -0500

    json querent implementation added

--- src/services/json/JsonQuerent.ts
+++ src/services/json/JsonQuerent.ts
@@ -1,17 +1,26 @@
 import { QueryResponse } from "../../types/QueryResponse";
 import { IContentQuerent } from "../IContentQuerent";
+import jsonpath from "jsonpath";
 
 export class JsonQuerent implements IContentQuerent {
   queryContent(content: string, queries: string[]): QueryResponse[] {
-    return queries.map((query) => this.processQuery(content, query));
+    const json = JSON.parse(content);
+    return queries.map((query) => this.processQuery(json, query));
   }
 
-  private processQuery(content: string, query: string): QueryResponse {
+  private processQuery(json: any, query: string): QueryResponse {
+    let result = jsonpath.query(json, query);
+
+    // Convert single result to an array for consistency
+    if (!Array.isArray(result)) {
+      result = result ? [result] : [];
+    }
+
     let queryResponse: QueryResponse = {
-      isValid: false,
-      stringValue: "",
-      isNodeArray: false,
-      nodes: [],
+      isValid: result.length > 0,
+      stringValue: JSON.stringify(result, null, 2),
+      isNodeArray: Array.isArray(result) && result.length > 1,
+      nodes: result,
     };
 
     return queryResponse;
